7d8b8d1377b559474c0f75ee5957788428199849
Rawaata14 <rawatareef1@gmail.com>
2025-04-11 10:59:15 +0300

    Task3

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,26 @@
 'use strict';
-//Task#1
-if ("0") {
-    alert('Hello');
-}
-//כן יהיה alert 
+// //Task#1
+// if ("0") {
+//     alert('Hello');
+// }
+// //כן יהיה alert 
 
-//Task#2
-let trueAnswer = 'ECMAScript';
-let answer = prompt("What is the official name of JavaScript?");
-if (answer == trueAnswer) {
-    alert('Correct!')
-}
-else {
-    alert("You don't know?! , It's ECMAScript!")
-}
+// //Task#2
+// let trueAnswer = 'ECMAScript';
+// let answer = prompt("What is the official name of JavaScript?");
+// if (answer == trueAnswer) {
+//     alert('Correct!')
+// }
+// else {
+//     alert("You don't know?! , It's ECMAScript!")
+// }
+
+//Task#3
+let score = 75;
+let grade = score >= 90 ? 'A' :
+    score >= 80 ? 'B' :
+        score >= 70 ? 'C' :
+            score >= 60 ? 'D' : 'F';
+
+console.log("The grade is: ", grade);
+
